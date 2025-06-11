be5d42383628342e6d8be5ea0f0bc31e19f071b3
Lucky Logic Developer <dev@luckylogic.com>
2025-06-11 21:29:29 +1000

    Update project configurations and dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 // app/layout.tsx
 'use client';
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 import theme from '../theme/theme';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="r71WszO964Ylg5Pg3MBa_q3h5x0o53j4FBd5v6gUWgk" />
+      </Head>
       <body>
         <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </body>
