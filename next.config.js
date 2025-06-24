28c8794adc28bd1090c699ca0cdaaf57ce928ebb
Lucky Logic Developer <dev@luckylogic.com>
2025-06-25 02:29:37 +1000

    test

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const cspHeader = `
     script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.crisp.chat https://www.googletagmanager.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/;
     style-src 'self' 'unsafe-inline' https://*.crisp.chat https://fonts.googleapis.com;
     img-src 'self' data: https://*.crisp.chat https://www.google-analytics.com https://www.googletagmanager.com https://www.gstatic.com/recaptcha/ https://www.google.com/recaptcha/;
-    font-src 'self' https://fonts.gstatic.com;
+    font-src 'self' https://fonts.gstatic.com https://client.crisp.chat;
     connect-src 'self' https://*.crisp.chat wss://*.crisp.chat https://www.google-analytics.com https://formspree.io/ https://www.google.com/;
     frame-src 'self' https://*.crisp.chat https://www.google.com/recaptcha/;
     worker-src 'self' blob:;
